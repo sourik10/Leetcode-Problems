ecdbd4ea5034832e01c6c2cb824c12b3c0c98cd8
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2023-07-30 02:10:36 +0530

    Create twoSum.cpp

---
+++ twoSums.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>>rs;
+        for(int i=0;i<nums.size();i++){
+            rs.push_back(make_pair(nums[i],i));
+        }
+        sort(rs.begin(),rs.end());
+        int l=0,h=nums.size()-1;
+        while(l<=h){
+            if(rs[l].first+rs[h].first==target) return {rs[l].second,rs[h].second};
+            else if(rs[l].first+rs[h].first < target) l++;
+            else h--;
+        }
+        return {-1,-1};
+    }
+};
