59f51b6864604d4fa2e128ff57826c9f711f22b5
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2023-07-31 10:21:31 +0530

    Create majorityElement.cpp

---
+++ majorityElement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans;
+        int n=nums.size()/2;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto it :mp){
+            if(it.second > n) {
+                 ans=it.first;
+            }
+        }
+        return ans;
+    }
+};
