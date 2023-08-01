92babdff3eff60c28cf33cbb50b763db34e45da0
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2023-08-01 11:48:31 +0530

    Update gfgPatterns.cpp

--- gfgPatterns.cpp
+++ gfgPatterns.cpp
@@ -1,4 +1,5 @@
 
+//Striver Pattern Series
 //Pattern-1: Rectangular Star Pattern
 void printSquare(int n) {
         for(int i=0;i<n;i++){
@@ -100,6 +101,83 @@ void printTriangle(int n) {
 	    
 	}
 
+//Pattern – 11: Binary Number Triangle Pattern
+void printTriangle(int n) {
+        
+        for(int i=0;i<n;i++){
+            int no=(i%2)?0:1;
+            for(int j=0;j<=i;j++){
+                if(no==1){
+                    cout<<no<<" ";
+                    no=0;
+                }
+                else{
+                    cout<<no<<" ";
+                    no=1;
+                }
+            }
+            cout<<endl;
+        }
+
+//Pattern – 12: Number Crown Pattern
+void printTriangle(int N) {
+         int spaces = 2*(N-1);
+      
+      // Outer loop for the number of rows.
+      for(int i=1;i<=N;i++){
+          // for printing numbers in each row
+          for(int j=1;j<=i;j++){
+              cout<<j;
+          }
+          // for printing spaces in each row
+          for(int j = 1;j<=(2*N)-(2*i);j++){
+              cout<<" ";
+          }
+          // for printing numbers in each row
+          for(int j=i;j>=1;j--){
+              cout<<j;
+          }
+          cout<<endl;
+      }
+    }
+
+//Pattern – 13: Increasing Number Triangle Pattern
+void printTriangle(int n) {
+        
+        int k=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<k++<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+//Pattern-14: Increasing Letter Triangle Pattern
+void printTriangle(int n) {
+        
+        
+        for(int i=0;i<n;i++){
+            char ch='A';
+            for(int j=0;j<=i;j++){
+                cout<<ch++;
+            }
+            cout<<endl;
+        }
+    }
+
+//Pattern-15: Reverse Letter Triangle Pattern
+void printTriangle(int n) {
+        for(int i=0;i<n;i++){
+            char ch='A';
+            for(int j=0;j<n-i;j++){
+                cout<<ch++;
+            }
+            cout<<endl;
+            ch='A';
+        }
+    }
+
 
 //Pattern – 16: Alpha-Ramp Pattern
 void printTriangle(int n) {
@@ -151,6 +229,8 @@ void printTriangle(int n) {
         }
     }
 
+//Pattern-19: Symmetric-Void Pattern
+//Pattern – 20: Symmetric-Butterfly Pattern
 
 //Pattern – 21: Hollow Rectangle Pattern
 void printSquare(int n) {
@@ -162,3 +242,6 @@ void printSquare(int n) {
             cout<<endl;
         }
     }
+
+//Pattern – 22: The Number Pattern
+
